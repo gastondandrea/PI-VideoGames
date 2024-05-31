b901c7f84eb935a08d6c83d4d350eec557070ed3
gastondandrea <gastondandrea@hotmail.com>
2024-05-30 21:00:53 -0300

    camibos bdd

--- api/src/db.js
+++ api/src/db.js
@@ -2,11 +2,16 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const {DB_USER, DB_PASSWORD, DB_HOST, DB_PORT} = process.env;
+const {DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_DEPLOY} = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/videogames`, {
+const sequelize = new Sequelize(DB_DEPLOY, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  dialectOptions:{
+    ssl: {
+      require: true,
+    }
+  }
 });
 const basename = path.basename(__filename);
 
